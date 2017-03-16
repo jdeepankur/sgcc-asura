0cf590eddb2d96bedfddd78da12229d5f32a3b27
Deepankur John N <jdeepankur@outlook.com>
2017-03-16 19:26:20 +0800

    Nothing to see here

---
+++ Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public static float speedModifier = 1.0f;
+	// Use this for initialization
+	void Start () {
+		speedModifier = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Project/Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+	public float speed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.x -= speed*Time.deltaTime * GameController.speedModifier;
+		if (transform.position.x < -5f) {
+			pos.x += 13f;
+		}
+		transform.position = pos;
+	}
+
+}
